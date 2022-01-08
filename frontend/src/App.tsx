e5157b82ee0836f0c0a320fe454c8bed100019bb
Leandro Toledo <lgtoledo@outlook.com>
2022-01-08 01:04:27 -0300

    Cambiamos AskPage y  su Route en App para que la misma se cargue bajo demanda(Lazy loading)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,16 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import React from 'react';
 import logo from './logo.svg';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
 import { fontFamily, fontSize, gray2 } from './Styles';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AskPage } from './AskPage';
 import { SearchPage } from './SearchPage';
 import { SignInPage } from './SignInPage';
 import { NotFoundPage } from './NotFoundPage';
 import { QuestionPage } from './QuestionPage';
+const AskPage = React.lazy(() => import('./AskPage'));
 
 function App() {
   return (
@@ -24,7 +25,25 @@ function App() {
         <Header />
         <Routes>
           <Route path="" element={<HomePage />} />
-          <Route path="ask" element={<AskPage />} />
+          <Route
+            path="ask"
+            element={
+              <React.Suspense
+                fallback={
+                  <div
+                    css={css`
+                      margin-top: 100px;
+                      text-align: center;
+                    `}
+                  >
+                    Loading...
+                  </div>
+                }
+              >
+                <AskPage />
+              </React.Suspense>
+            }
+          />
           <Route path="search" element={<SearchPage />} />
           <Route path="signin" element={<SignInPage />} />
           <Route path="questions/:questionId" element={<QuestionPage />} />
