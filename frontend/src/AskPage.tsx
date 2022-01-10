0f477e3f8dccd89478070cc6db8f52ddddeab637
Leandro Toledo <lgtoledo@outlook.com>
2022-01-10 19:47:03 -0300

    Se implementa el form submit para la funcionalidad Ask a Question

--- frontend/src/AskPage.tsx
+++ frontend/src/AskPage.tsx
@@ -9,8 +9,10 @@ import {
   FormButtonContainer,
   PrimaryButton,
   FieldError,
+  SubmissionSuccess,
 } from './Styles';
-import { useForm } from 'react-hook-form';
+import { useForm, useFormState } from 'react-hook-form';
+import { postQuestion } from './QuestionsData';
 
 type FormData = {
   title: string;
@@ -18,16 +20,27 @@ type FormData = {
 };
 
 export const AskPage = () => {
-  const {
-    register,
-    formState: { errors },
-  } = useForm<FormData>({
+  const [successfullySubmitted, setSuccessfullySubmitted] =
+    React.useState(false);
+
+  const { register, formState, handleSubmit } = useForm<FormData>({
     mode: 'onBlur',
   });
+
+  const submitForm = async (data: FormData) => {
+    const result = await postQuestion({
+      title: data.title,
+      content: data.content,
+      userName: 'Fred',
+      created: new Date(),
+    });
+    setSuccessfullySubmitted(result ? true : false);
+  };
+
   return (
     <Page title="Ask a question">
-      <form>
-        <Fieldset>
+      <form onSubmit={handleSubmit(submitForm)}>
+        <Fieldset disabled={formState.isSubmitted || successfullySubmitted}>
           <FieldContainer>
             <FieldLabel htmlFor="title">Title</FieldLabel>
             <FieldInput
@@ -38,12 +51,16 @@ export const AskPage = () => {
               })}
               type="text"
             />
-            {errors.title && errors.title.type === 'required' && (
-              <FieldError>You must enter the question title</FieldError>
-            )}
-            {errors.title && errors.title.type === 'minLength' && (
-              <FieldError>The title must be at least 10 characters</FieldError>
-            )}
+            {formState.errors.title &&
+              formState.errors.title.type === 'required' && (
+                <FieldError>You must enter the question title</FieldError>
+              )}
+            {formState.errors.title &&
+              formState.errors.title.type === 'minLength' && (
+                <FieldError>
+                  The title must be at least 10 characters
+                </FieldError>
+              )}
           </FieldContainer>
           <FieldContainer>
             <FieldLabel htmlFor="content">Content</FieldLabel>
@@ -54,18 +71,25 @@ export const AskPage = () => {
                 minLength: 50,
               })}
             />
-            {errors.content && errors.content.type === 'required' && (
-              <FieldError>You must enter the question content</FieldError>
-            )}
-            {errors.content && errors.content.type === 'minLength' && (
-              <FieldError>
-                The content must be at least 50 characters
-              </FieldError>
-            )}
+            {formState.errors.content &&
+              formState.errors.content.type === 'required' && (
+                <FieldError>You must enter the question content</FieldError>
+              )}
+            {formState.errors.content &&
+              formState.errors.content.type === 'minLength' && (
+                <FieldError>
+                  The content must be at least 50 characters
+                </FieldError>
+              )}
           </FieldContainer>
           <FormButtonContainer>
             <PrimaryButton type="submit">Submit Your Question</PrimaryButton>
           </FormButtonContainer>
+          {successfullySubmitted && (
+            <SubmissionSuccess>
+              Your question was successfully submitted
+            </SubmissionSuccess>
+          )}
         </Fieldset>
       </form>
     </Page>
