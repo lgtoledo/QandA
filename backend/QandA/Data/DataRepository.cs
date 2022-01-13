6254d521373e2e62dad47a046debbc8094ef52ac
Leandro Toledo <lgtoledo@outlook.com>
2022-01-13 10:40:56 -0300

    Implementamos la interface y el esqueleto de la clase para el respositorio (DataRepository)

---
+++ backend/QandA/Data/DataRepository.cs
@@ -0,0 +1,35 @@
+﻿namespace QandA.Data
+{
+    public class DataRepository : IDataRepository
+    {
+        public AnswerGetResponse GetAnswer(int answerId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public QuestionGetSingleResponse GetQuestion(int questionId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public System.Collections.Generic.IEnumerable<QuestionGetManyResponse> GetQuestions()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public System.Collections.Generic.IEnumerable<QuestionsGetManyResponse> GetQuestionsBySearch(string search)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public System.Collections.Generic.IEnumerable<QuestionsGetManyResponse> GetUnansweredQuestions()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool QuestionExists(int questionId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
