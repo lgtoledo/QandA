e00bd5b44aa37b785d4f5496b7635dc67df7d3fe
Leandro Toledo <lgtoledo@outlook.com>
2022-01-20 09:34:33 -0300

    Se crea el API controller QuestionsController

---
+++ backend/QandA/Controllers/QuestionsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QandA.Data;
+using QandA.Data.Models;
+
+namespace QandA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        private readonly IDataRepository _dataRepository;
+
+        public QuestionsController(IDataRepository dataRepository)
+        {
+            _dataRepository = dataRepository;
+        }
+    }
+}
