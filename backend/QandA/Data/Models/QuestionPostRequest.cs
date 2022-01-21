9fd8f425ed7342a9b01795e0d46ef06a82e7801c
Leandro Toledo <lgtoledo@outlook.com>
2022-01-21 14:58:12 -0300

    Se agregan validaciones a algunos campos

--- backend/QandA/Data/Models/QuestionPostRequest.cs
+++ backend/QandA/Data/Models/QuestionPostRequest.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace QandA.Data.Models
 {
     public class QuestionPostRequest
     {
+        [Required]
+        [StringLength(100)]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Por favor incluya algún contenido para la pregunta")]
         public string Content { get; set; }
         public string UserId { get; set; }
         public string UserName { get; set; }
