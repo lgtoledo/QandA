b8029a464571f67b6f938960acc1f1cf9e8a9b63
Leandro Toledo <lgtoledo@outlook.com>
2022-01-24 01:53:11 -0300

    Configuramos la caché para dependency injection, y la instanciamos con singleton para compartirla en toda la app por todo el tiempo que la misma dure

--- backend/QandA/Startup.cs
+++ backend/QandA/Startup.cs
@@ -49,6 +49,8 @@ namespace QandA
             });
 
             services.AddScoped<IDataRepository, DataRepository>();
+            services.AddMemoryCache();
+            services.AddSingleton<IQuestionCache, QuestionCache>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
